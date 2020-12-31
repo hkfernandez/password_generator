84740ef15cf150eb3fefe036fd8cba18aadc57f5
Hector Fernandez <hkfernandezdev@gmail.com>
2020-12-31 15:59:17 -0600

    bug fix - script would not end when user canceled password length prompt after haveing entered an incorrect value

--- script.js
+++ script.js
@@ -13,55 +13,46 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword () {
+  //building an array of arrays of character types based on user preference
+  //generate rondom indexes of the array and its nested arrays to select a random character
+  //add the random characters to the password
 
-//building an array of arrays of character types based on user preference
-//generate rondom indexes of the array and its nested arrays to select a random character
-//add the random characters to the password
+  var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var numbersArr = ["1","2","3","4","5","6","7","8","9"];
+  var specialCharArr = ["!", "@", "#", "$", "%", "&", "*", "?"];
+  var pwLength = 0;
+  var charArr = []
+  var lowerPrompt = "Would you like to use lowercase letters in your password? (y)es or (n)o"
+  var upperPrompt = "Would you like to use uppercase letters in your password? (y)es or (n)o"
+  var numPrompt = "Would you like to use numbers in your password? (y)es or (n)o"
+  var specialCharPrompt = "Would you like to use special characters in your password? (y)es or (n)o"
+  var passwordRandom = ""
+  var sessionCanceledAlert = "Session canceled"
 
-var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbersArr = ["1","2","3","4","5","6","7","8","9"];
-var specialCharArr = ["!", "@", "#", "$", "%", "&", "*", "?"];
-var pwLength = 0;
-var charArr = []
-var lowerPrompt = "Would you like to use lowercase letters in your password? (y)es or (n)o"
-var upperPrompt = "Would you like to use uppercase letters in your password? (y)es or (n)o"
-var numPrompt = "Would you like to use numbers in your password? (y)es or (n)o"
-var specialCharPrompt = "Would you like to use special characters in your password? (y)es or (n)o"
-var password = ""
-sessionStatus = "active"
-pwLength = 0
-sessionCanceledAlert = "Session canceled"
+  alert ("We'll help you set up a randomly generated password based on your preferences. Please answer the following questions.");
 
-// function userPromptCancel ( userChoice ) {
-//   if ( userChoice === null ) {
-//     alert ("Session canceled.");
-//     sessionStatus = "inactive";
-//     console.log (sessionStatus);
-// }
-// userPromptCancel ( pwLength ) ;
-
-alert ("We'll help you set up a randomly generated password on your preferences. Please answer the following questions.") 
-
-while (sessionStatus = "active") {
-    
   // Checks if user entry for password length is in range and is a number
-  function lengthValidate (numLength) {
-    while (numLength < 8 || numLength > 128 || isNaN ( numLengthStr)) {
-      var numLengthStr = prompt ("Enter your prefered length of password from 8 to 128 characters");
-      if ( numLengthStr == null ) {
-        break;
+  function lengthValidate (userInput) {
+    while (userInput < 8 || userInput > 128 || isNaN (userInput) || userInput == null) {
+      var userInput = prompt ("Enter your prefered length of password from 8 to 128 characters");
+      if (userInput == null) {
+        return;
       }
-      var numLength = parseInt ( numLengthStr ) ;
+      var numLength = parseInt (userInput) ;
     }
-    return numLength;
+    if (numLength == NaN) {
+      return undefined;
+    } else {
+      return numLength;
+    }
   }
+  
   var pwLength = lengthValidate (pwLength);
-  if ( pwLength === 0 ) {
-    sessionStatus = "inactive";
-    console.log (sessionStatus);
-    alert ( sessionCanceledAlert );
-    break;
+  if (pwLength === undefined) {
+    alert (sessionCanceledAlert);
+    return;
   }
 
   //checks is user entry is a "y" or "n"
@@ -73,24 +64,19 @@ while (sessionStatus = "active") {
       }
     }
     if (userInput == "y" || userInput == "n"){
-      console.log ("user input" + userInput)
       return userInput;
     } else {
       return null;
     }
   }
 
-  //validates users choices and pushes them to the arrays
+  //validates users choices and pushes them to the character array
   var lowerChoice = yesNoValidate (lowerChoice, lowerPrompt);
   if (lowerChoice == "y") {
   charArr.push (lowerArr);
   alert ("Lowercase characters added");
   } else if (lowerChoice == null) {
-    sessionStatus = "inactive"
-  }
-  if (sessionStatus != "active") {
-    alert (sessionCanceledAlert);
-    break;
+    return;
   }
 
   var upperChoice = yesNoValidate (upperChoice, upperPrompt);
@@ -98,11 +84,7 @@ while (sessionStatus = "active") {
   charArr.push (upperArr);
   alert ("Uppercase characters added");
   } else if (upperChoice == null) {
-    sessionStatus = "inactive"
-  }
-  if (sessionStatus != "active") {
-    alert (sessionCanceledAlert);
-    break;
+    return;
   }
 
   var numChoice = yesNoValidate (numChoice, numPrompt);
@@ -110,11 +92,7 @@ while (sessionStatus = "active") {
   charArr.push (numbersArr);
   alert ("Numbers added");
   } else if (numChoice == null) {
-    sessionStatus = "inactive"
-  }
-  if (sessionStatus != "active") {
-    alert (sessionCanceledAlert);
-    break;
+    return;
   }
 
   var specialCharChoice = yesNoValidate (specialCharChoice, specialCharPrompt);
@@ -122,34 +100,25 @@ while (sessionStatus = "active") {
   charArr.push (specialCharArr);
   alert ("Special characters added");
   } else if (specialCharChoice == null) {
-    sessionStatus = "inactive"
-  }
-  if (sessionStatus != "active") {
-    alert (sessionCanceledAlert);
-    break;
+    return;
   }
 
-  alert ("Great! That is all the information we needed to create your password. Click on the generate password button below.");
-  break;
-}
   //returns a random index of the array it is called on
   function arrIndexGen (arr) {
   return Math.floor( Math.random () * arr.length);
   }
-
 
   //returns a random index of the array inside the array it is called on
   function nestArrIndexGen (arr, arrIndex) {
     return Math.floor( Math.random () * arr[arrIndex].length) ;
   }
 
-
+  //loops through array of character types and randomly selects characters
   for (i=0; i < pwLength; i++) {
     var charTypeIndex =  arrIndexGen (charArr);
     var charIndex = nestArrIndexGen (charArr, charTypeIndex);
-    password += charArr [charTypeIndex] [charIndex] ;
+    passwordRandom += charArr [charTypeIndex] [charIndex] ;
   };
-  console.log (password);
-  sessionStatus = "inactive";
-  console.log (sessionStatus)
 
+  return passwordRandom;
+}
